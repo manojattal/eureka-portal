2df0c73f2d7f5745103f67a2e64c0d3d0034acfa
Manoj Attal <manoj.attal@redknee.com>
2017-05-15 16:03:18 +0530

    First Commit

---
+++ ePortal/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ePortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult TermsConditions()
+        {
+            return View();
+        }
+
+        public ActionResult SmartHome()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Solutions()
+        {
+            return View();
+        }
+
+        public ActionResult Estimate()
+        {
+            return View();
+        }
+    }
+}
---
+++ ePortal/Scripts/slidermanager.js
@@ -0,0 +1,10 @@
+﻿$.getScript('slider.js', function () {
+    $(document).omSlider({
+        slider: $('.slider'),
+        dots: $('.dots'),
+        next: $('.btn-right'),
+        pre: $('.btn-left'),
+        timer: 5000,
+        showtime: 1000
+    });
+});
